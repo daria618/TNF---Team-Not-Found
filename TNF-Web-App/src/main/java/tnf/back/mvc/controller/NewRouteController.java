1f2761d3274195817e88b8d29f417613b8f6f737
ZNickel <Qwfsk@yandex.ru>
2023-05-02 08:43:03 +0500

    extend route card, add base comment logic

--- TNF-Web-App/src/main/java/tnf/back/mvc/controller/NewRouteController.java
+++ TNF-Web-App/src/main/java/tnf/back/mvc/controller/NewRouteController.java
@@ -16,48 +16,47 @@ public class NewRouteController {
 
     @GetMapping("/editor/create")
     public String OpenEditorCreate(){
-        return "temp_route_editor";
+        return "create_route";
     }
 
-    @GetMapping("/editor/edit")
-    public String OpenEditorEdit(Model model){
-        return "temp_route_editor";
-    }
-
-    @RequestMapping(value = "/editor/create", method = RequestMethod.POST)
-    public String acceptForm(HttpServletRequest request, Model model){
-        Map<String, String[]> parameterMap = request.getParameterMap();
-
-        ArrayList<MapPoint> points = new ArrayList<>(16);
-
-        ArrayList<String> messages = new ArrayList<>(16);
-
-        parameterMap.forEach((key, value) -> {
-            if (!key.equals("_csrf")){
-                String[] keyParts = key.split("_");
-                if (keyParts.length == 4){
-                    int index = Integer.parseInt(keyParts[3]);
-                    if (index >= points.size()) points.add(new MapPoint());
-                    switch (keyParts[1]){
-                        case "name" -> points.get(index).setTextRepresent(value[0]);
-                        case "lat" -> points.get(index).setLatitude(value[0]);
-                        case "lon" -> points.get(index).setLongitude(value[0]);
-                    }
-                }
-
-            }
-            StringBuilder builder = new StringBuilder(key + " -> |");
-            for (var v : value)
-                builder.append(v).append("|");
-            messages.add(builder.toString());
-        });
-
-        model.addAttribute("messages", messages);
-        model.addAttribute("points", points);
-
-        return "temp_route_editor";
-    }
-
+//    @GetMapping("/editor/edit")
+//    public String OpenEditorEdit(Model model){
+//        return "create_route";
+//    }
+//
+//    @RequestMapping(value = "/editor/create", method = RequestMethod.POST)
+//    public String acceptForm(HttpServletRequest request, Model model){
+//        Map<String, String[]> parameterMap = request.getParameterMap();
+//
+//        ArrayList<MapPoint> points = new ArrayList<>(16);
+//
+//        ArrayList<String> messages = new ArrayList<>(16);
+//
+//        parameterMap.forEach((key, value) -> {
+//            if (!key.equals("_csrf")){
+//                String[] keyParts = key.split("_");
+//                if (keyParts.length == 4){
+//                    int index = Integer.parseInt(keyParts[3]);
+//                    if (index >= points.size()) points.add(new MapPoint());
+//                    switch (keyParts[1]){
+//                        case "name" -> points.get(index).setTextRepresent(value[0]);
+//                        case "lat" -> points.get(index).setLatitude(value[0]);
+//                        case "lon" -> points.get(index).setLongitude(value[0]);
+//                    }
+//                }
+//
+//            }
+//            StringBuilder builder = new StringBuilder(key + " -> |");
+//            for (var v : value)
+//                builder.append(v).append("|");
+//            messages.add(builder.toString());
+//        });
+//
+//        model.addAttribute("messages", messages);
+//        model.addAttribute("points", points);
+//
+//        return "temp_route_editor";
+//    }
 
 
 }
--- TNF-Web-App/src/main/resources/static/script/addInput.js
+++ TNF-Web-App/src/main/resources/static/script/addInput.js
@@ -1,3 +1,5 @@
+var blocks = [];
+
 function addPointCoords() {
     const form = document.getElementById("route_configurator");
 
@@ -6,19 +8,28 @@ function addPointCoords() {
     lon.classList.add("cord_lon")
     lon.classList.add("indexed")
     lon.placeholder = "Долгота";
+    lon.style.width = "50%";
     lon.name = "cord_lon_ind_" + (form.getElementsByClassName("indexed").length);
 
     const lat = document.createElement("input");
     lat.type = "text";
     lat.classList.add("cord_lat")
-    lon.classList.add("indexed")
+    lat.classList.add("indexed")
     lat.placeholder = "Широта";
+    lat.style.width = "50%";
     lat.name = "cord_lat_ind_" + (form.getElementsByClassName("indexed").length);
 
     const newDiv = document.createElement("div");
+    newDiv.classList.add("border");
+    newDiv.classList.add("p-3");
+
+    const del = document.createElement("button");
+    del.onclick = ev => document.removeChild(newDiv);
+    del.textContent = "Delete";
 
     newDiv.appendChild(lon)
     newDiv.appendChild(lat)
+    newDiv.appendChild(del)
     form.appendChild(newDiv);
 }
 function addPointName(){
@@ -31,9 +42,15 @@ function addPointName(){
     name.name = "name_name_ind_" + (form.getElementsByClassName("indexed").length);
     const newDiv = document.createElement("div");
     newDiv.appendChild(name)
+    newDiv.classList.add("border");
+    newDiv.classList.add("p-3");
     form.appendChild(newDiv);
 }
 
 function selectPoint(){
+
+}
+
+function create(){
 
 }
